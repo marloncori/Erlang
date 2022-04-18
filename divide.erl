476e3e4b0f39b6434563374a056a11c26e0878ff
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-18 23:58:51 +0200

    Update divide.erl

--- divide.erl
+++ divide.erl
@@ -1,14 +1,13 @@
--module(helloerlang).
+-module(divide).
 -author("Millenium Robotics").
 -version("1.0").
 -export([div/2, start/0]).
 
-div(X, Y) when Y > 0 ->
+div(X, Y) when Y>0 ->
     Q = X / Y,
     io:fwrite("~w~n", [Q]).
 
 start() ->
     div(45, 3),
     div(98, 2),
-    % this one throws an error
     div(13, 0).
