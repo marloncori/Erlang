cfcdcb11b8b68beb5260e2a81b4c06022f35b11e
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-18 23:59:29 +0200

    Update dropLast.erl

--- dropLast.erl
+++ dropLast.erl
@@ -1,4 +1,4 @@
--module(helloerlang).
+-module(dropLast).
 -author("Millenium Robotics").
 -version("1.0").
 -import(lists, [droplast/1]).
@@ -6,7 +6,6 @@
 
 start() ->
     List1 = [2, 3, 4, 5],
-    List2 = droplast(Lst1),
-    % prints [2, 3, 4]
+    List2 = droplast(List1),
     io:fwrite("~w~n", [List2]).
     
