f983fc72aff3f2e4d72653b9f310596c30b73b82
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-18 23:51:39 +0200

    Update bitstring.erl

--- bitstring.erl
+++ bitstring.erl
@@ -1,10 +1,10 @@
--module(helloerlang).
+-module(bitstring).
 -author("Millenium Robotics").
 -version("1.0").
 -export([start/0]).
 
 start() ->
     Binary = <<10, 20>>,
-    L = binary_to_list,
+    L = binary_to_list(Binary),
     % prints [10, 20]
     io:fwrite("~w~n", [L]).
