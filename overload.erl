633f244855f18ff991e9f2d6d8b2ac1e3d95ddae
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-19 00:21:59 +0200

    Update overload.erl

--- overload.erl
+++ overload.erl
@@ -1,20 +1,19 @@
 -module(helloerlang).
 -author("Millenium Robotics").
 -version("1.0").
--import(io, [fwrite/1]).
 -export([add/4, add/3, add/2, start/0]).
 
 add(X, Y, Z, W) -> 
     Result = X + Y + Z + W,
-    fwrite("~w~n", [Result]).
+    io:fwrite("~w~n", [Result]).
 
 add(X, Y, Z) -> 
     Result = X + Y + Z,
-    fwrite("~w~n", [Result]).
+    io:fwrite("~w~n", [Result]).
 
 add(X, Y) -> 
     Result = X + Y,
-    fwrite("~w~n", [Result]).
+    io:fwrite("~w~n", [Result]).
 
 % recursive
 % add([]) -> 0;
@@ -25,6 +24,6 @@ start() ->
     B = 132,
     C = 78,
     D = 23,
-    add(B, A, D, C).
-    add(A, D, C).
+    add(B, A, D, C),
+    add(A, D, C),
     add(A, B).
