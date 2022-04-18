04307cda66bb480c51062a73d3c966293ac115ec
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-19 00:00:20 +0200

    Update duplicate.erl

--- duplicate.erl
+++ duplicate.erl
@@ -1,4 +1,4 @@
--module(helloerlang).
+-module(duplicate).
 -author("Millenium Robotics").
 -version("1.0").
 -import(lists, [duplicate/2]).
@@ -7,5 +7,4 @@
 % syntax duplicate(repetition, element)
 start() ->
     List1 = duplicate(5, 1),
-    % prints [1, 1, 1, 1, 1]
     io:fwrite("~w~n", [List1]).
