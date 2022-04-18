81d389f0f91ae3fb93d2728a7451f3ddeb92a107
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-18 23:41:12 +0200

    Update arccos.erl

--- arccos.erl
+++ arccos.erl
@@ -1,9 +1,9 @@
--module(helloerlang).
+-module(arccos).
 -author("Millenium Robotics").
 -version("1.0").
 -import(math, [acos/1]).
 -export([start/0]).
 
 start() ->
-    Arccos = sin(0.7071),
+    Arccos = acos(0.7071),
     io:fwrite("~p~n", [Arccos]).
