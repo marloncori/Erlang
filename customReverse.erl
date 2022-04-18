10b8715d8b35937b9527b672c847964979b2f200
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-18 23:56:53 +0200

    Update customReverse.erl

--- customReverse.erl
+++ customReverse.erl
@@ -1,8 +1,8 @@
--module(helloerlang).
+-module(customReverse).
 -author("Millenium Robotics").
 -version("1.0").
--import(io, [fwrite/1]).
--export([add/4, add/3, add/2, start/0]).
+-import(io, [fwrite/2]).
+-export([rev/1, rev/2, start/0]).
 
 rev(L) -> rev(L, []);
 rev([], Acc) -> Acc;
@@ -11,4 +11,4 @@ rev([H|T], Acc) -> rev(T, [H|Acc]).
 start() ->
     List = [2, 3, 4, 5, 7, 8, 9, 10],
     New = rev(List),
-    fwrite(~w~n", [New]).
+    fwrite("~w~n", [New]).
