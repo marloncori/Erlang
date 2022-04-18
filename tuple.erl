fbdfc8abf334ed4bc4054c4e8cc3d323c24d3396
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-19 00:26:44 +0200

    Update tuple.erl

--- tuple.erl
+++ tuple.erl
@@ -1,9 +1,8 @@
--module(helloerlang).
+-module(tuple).
 -author("Millenium Robotics").
 -version("1.0").
--import(io, [fwrite/1]).
 -export([start/0]).
 
 start() ->
     Person = {john, 25, {september, 22}},
-    fwrite(~w~n", [tuple_size(Person)]).
+    io:fwrite(~w~n", [tuple_size(Person)]).
