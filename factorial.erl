51b0c77c8bdf5dd794cd2143f3a4ba154febf056
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-19 00:03:24 +0200

    Update factorial.erl

--- factorial.erl
+++ factorial.erl
@@ -1,11 +1,11 @@
--module(helloerlang).
+-module(factorial).
 -author("Millenium Robotics").
 -version("1.0").
--import(io, [fwrite/1]).
+-import(io, [fwrite/2]).
 -export([fact/1, start/0]).
 
 fact(N) when N == 0 -> 1;
-fact(N) when N > 0 -> N * fac(N-1);
+fact(N) when N > 0 -> N * fact(N-1).
 
 start() ->
     X = fact(5),
