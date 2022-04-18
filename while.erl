a3866ca020315ec488a4f115787f53e98d1720d1
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-19 00:27:54 +0200

    Update while.erl

--- while.erl
+++ while.erl
@@ -1,13 +1,13 @@
 -module(helloerlang).
 -author("Millenium Robotics").
 -version("1.0").
--import(io, [fwrite/1]).
 -export([while/1, while/2, start/0]).
 
 while(L) -> while(L, 0).
+
 while([], Acc) -> Acc;
 while([_|T], Acc) ->
-    fwrite("~w~n", [Acc]),
+    io:fwrite("~w~n", [Acc]),
     while(T, Acc+1).
 
 start() ->
