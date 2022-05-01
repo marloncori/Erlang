9cefbc3bec3722cf79989ec4a0740bfca2570f17
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-05-02 01:10:23 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+error_msg=" WARNING: file name is missing! There is nothing to be compiled!"
 file=$1
-
-echo " Running the application..."
-sleep 1
-erl -noshell -s $file start -s init stop
-sleep 1
-echo " Program successfully executed!"
+   
+ if[[ !$file ]]; then
+    echo $error_msg
+    exit
+ fi
+    
+ echo " Compiling file..."
+ erlc $file.erl
+ Sleep 1
+ echo " Running the application..."
+ sleep 1
+ erl -noshell -s $file start -s init stop
+ sleep 1
+ echo " Program successfully executed!"
